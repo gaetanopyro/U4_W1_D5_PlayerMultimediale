d81132073c0bafcdb3b8480e4134069a7470172f
gaetanopyro <gaetano.miscio@gmail.com>
2025-06-20 17:00:04 +0200

    add arrayy

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,62 @@
+import entites.ElementoMultimediale;
 import entites.Immagine;
 import entites.RegistrazioneAudio;
 import entites.Video;
 
+import java.util.Scanner;
+
 public class Main {
+
+
     public static void main(String[] args) {
 
-        RegistrazioneAudio song = new RegistrazioneAudio("Titolo traccia", 1, 10);
+        Scanner scanner = new Scanner(System.in);
+        ElementoMultimediale[] elementi = new ElementoMultimediale[5];
+
+        System.out.println("Digita 0 per Registrazione Audio, 1 per Video, 2 per Immagine.");
+
+        int i = 0;
+        while (i < 5) {
+            System.out.println("Inserisci il nome del file multimediale " + (i + 1) + " - scegli il tipo:");
+            String text = scanner.nextLine();
+
+            if (text.equals("0") || text.equals("1") || text.equals("2")) {
+                int tipo = Integer.parseInt(text);
+
+                if (tipo == 0) {
+                    System.out.println("Titolo traccia:");
+                    String titolo = scanner.nextLine();
+                    System.out.println("Durata:");
+                    int durata = Integer.parseInt(scanner.nextLine());
+                    System.out.println("Volume:");
+                    int volume = Integer.parseInt(scanner.nextLine());
+                    elementi[i] = new RegistrazioneAudio(titolo, durata, volume);
+                } else if (tipo == 1) {
+                    System.out.println("Titolo:");
+                    String titolo = scanner.nextLine();
+                    System.out.println("Durata:");
+                    int durata = Integer.parseInt(scanner.nextLine());
+                    System.out.println("Volume:");
+                    int volume = Integer.parseInt(scanner.nextLine());
+                    System.out.println("Luminosità:");
+                    int luminosita = Integer.parseInt(scanner.nextLine());
+                    elementi[i] = new Video(titolo, durata, volume, luminosita);
+                } else {
+                    System.out.println("Titolo:");
+                    String titolo = scanner.nextLine();
+                    System.out.println("Luminosità:");
+                    int luminosita = Integer.parseInt(scanner.nextLine());
+                    elementi[i] = new Immagine(titolo, luminosita);
+                }
+                i++;
+            } else {
+                System.out.println(" riprova.");
+            }
+        }
+    }
+}
+
+      /*  RegistrazioneAudio song = new RegistrazioneAudio("Titolo traccia", 1, 10);
         System.out.println("Test Volume");
         song.start();
         song.menoVolume();
@@ -15,10 +66,7 @@ public class Main {
         song.menoVolume();
         song.start();
         song.menoVolume();
-        song.start();
-        song.plusVolume();
-        song.start();
-        song.plusVolume();
+        System.out.println("Test Video");
         Video grafica = new Video("Titolo traccia", 1, 10, 10);
         grafica.start();
         grafica.menoLuminosità();
@@ -28,8 +76,7 @@ public class Main {
         grafica.plusLuminosità();
         grafica.start();
         grafica.menoLuminosità();
-        grafica.start();
-        grafica.plusLuminosità();
+        System.out.println("Test Immagine");
         Immagine img = new Immagine("Album:", 10);
         img.show();
         img.menoLuminosità();
@@ -39,7 +86,9 @@ public class Main {
         img.menoLuminosità();
         img.show();
         img.plusLuminosità();
-        
 
-    }
-}
+       */
+
+
+
+
